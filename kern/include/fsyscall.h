30dbbea22dc1b199534e9e219429ad7fbae4487f
Jacob Budzis <jrbudzis@alumni.ubc.ca>
2017-02-11 20:48:20 -0800

    Cleaned up fsyscall.h

--- kern/include/fsyscall.h
+++ kern/include/fsyscall.h
@@ -1,17 +1,18 @@
 #ifndef _FSYSCALL_H_
 #define _FSYSCALL_H_
 
-
 #include <filetable.h>
 #include <kern/seek.h>
 
-
+/*
+* User-invoked system calls.
+*/
 int sys_open(const char *, int, int32_t *);
 int sys_close(int);
-int sys_write(int fd, const void *buf, size_t nbytes, int32_t *);
-int sys_read(int fd, void *buf, size_t buflen, ssize_t *);
-int sys_lseek(int fd, off_t pos, int whence, int32_t *, int32_t *);
-int sys_dup2(int oldfd, int newfd, int32_t *);
+int sys_write(int, const void *, size_t, int32_t *);
+int sys_read(int, void *, size_t, ssize_t *);
+int sys_lseek(int, off_t, int, int32_t *, int32_t *);
+int sys_dup2(int, int, int32_t *);
 int sys_chdir(const char *);
 int sys___getcwd(char *, size_t, int32_t *);
 
